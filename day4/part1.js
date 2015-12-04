2b1b5bd49322581f291d3a74a77b0b225b2605c9
Joseph Bryan <joemfb@gmail.com>
2015-12-04 01:46:57 -0500

    day4

---
+++ day4/part1.js
@@ -0,0 +1,33 @@
+var md5 = require('md5')
+
+function findHashSuffix(key) {
+  var suffix = 1;
+  var hash;
+
+  while (true) {
+    hash = md5(key + suffix)
+    if (hash.substring(0, 5) === '00000') {
+      return suffix
+    } else {
+      suffix++
+    }
+  }
+}
+
+function test() {
+  return [
+    findHashSuffix('abcdef') === 609043,
+    findHashSuffix('pqrstuv') === 1048970
+  ]
+  .reduce(function(a, b) {
+    return a && b
+  })
+}
+
+if (test()) {
+  console.log('tests pass!\n')
+  console.log('solution:')
+  console.log('    ' + findHashSuffix('yzbqklnj'))
+} else {
+  console.error('tests failed')
+}
---
+++ day4/part2.js
@@ -0,0 +1,18 @@
+var md5 = require('md5')
+
+function findHashSuffix(key) {
+  var suffix = 1;
+  var hash;
+
+  while (true) {
+    hash = md5(key + suffix)
+    if (hash.substring(0, 6) === '000000') {
+      return suffix
+    } else {
+      suffix++
+    }
+  }
+}
+
+console.log('solution:')
+console.log('    ' + findHashSuffix('yzbqklnj'))
